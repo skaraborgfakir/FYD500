abb01737e49676c7ab2f2e6ca0cd9c7f22f580b6
Stefan Niskanen Skoglund <stenis@student.chalmers.se>
2019-05-15 20:15:28 +0200

    tektronix ska rita en cirkel

---
+++ maj 15/cirkel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+function circle ()
+{
+    echo ""
+}
+
+function go_tek4010 ()
+{
+    echo ""
+}
+
+sleep 20
+echo -e "\007"
+
+#echo -e ""
+
+#trap "echo QUIT" SIGQUIT
+#trap "echo TERM" SIGTERM
+#trap "echo INTR" SIGINT
+
+read DIE
